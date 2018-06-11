5a2f5274a6dbd8cc6dcd8de61a6d6342fdccb13d
Ariel Kuźmiński <a.kuzminski@nowaera.pl>
2018-06-11 16:31:43 +0200

    Added basic component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Ariel", age: 27 },
+      { name: "Laura", age: 40 }
+    ],
+    otherState: "some other value"
+  };
+
+  switchNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Ariel", age: 27 },
+        { name: "Laura", age: 40 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 27 },
+        { name: "Laura", age: 40 }
+      ]
+    });
+  };
+
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={() => this.switchNameHandler("Test")}>
+          Switch name
+        </button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}
+        >
+          My hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'H1, I\'m a React App'));
